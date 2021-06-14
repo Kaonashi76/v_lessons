aa1d500bb6c6669009667287b3e7f2707e91eae1
Max Glotov <glotovmaks97@gmail.com>
2021-06-14 13:30:32 +0300

    add slider function

---
+++ 3_slider/app.js
@@ -0,0 +1,46 @@
+const upBtn = document.querySelector('.up-button');
+const downBtn = document.querySelector('.down-button');
+
+const containerHeight = document.querySelector('.container').clientHeight;
+const sidebar = document.querySelector('.sidebar');
+const mainSlide = document.querySelector('.main-slide');
+const slidesCount = mainSlide.querySelectorAll('div').length;
+
+let slideIndex = 0;
+
+sidebar.style.top = `-${(slidesCount - 1) * 100}vh`;
+
+let interval = sliderInterval()
+
+function sliderInterval() {
+ let interval = setInterval(() => {
+    changeSlide(slideIndex += 1);
+  }, 5000);
+
+  return interval
+}
+
+upBtn.addEventListener('click', () => {
+  changeSlide(slideIndex += 1)
+})
+
+downBtn.addEventListener('click', () => {
+  changeSlide(slideIndex -= 1)
+})
+
+function changeSlide(activeSlideIndex){
+
+  clearInterval(interval);
+
+  if (activeSlideIndex === slidesCount) {
+    slideIndex = 0;
+  }
+  if (activeSlideIndex < 0) {
+    slideIndex = slidesCount - 1;
+  }
+
+  mainSlide.style.transform = `translateY(-${slideIndex * containerHeight}px)`;
+  sidebar.style.transform= `translateY(${slideIndex * containerHeight}px)`;
+
+  //interval = sliderInterval()
+}
